8a1a2f0497efeebe944aa9476a6a0d6639025e45
Dylan Roberts <dtroberts@knights.ucf.edu>
2020-03-26 22:20:41 -0400

    Added API call for Retrieving all anchors in Cosmos

---
+++ Controllers/AnchorsController.cs
@@ -0,0 +1,92 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using Microsoft.AspNetCore.Mvc;
+using SharingService.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+namespace SharingService.Controllers
+{
+    [Route("api/anchors")]
+    [ApiController]
+    public class AnchorsController : ControllerBase
+    {
+        private readonly IAnchorKeyCache anchorKeyCache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AnchorsController"/> class.
+        /// </summary>
+        /// <param name="anchorKeyCache">The anchor key cache.</param>
+        public AnchorsController(IAnchorKeyCache anchorKeyCache)
+        {
+            this.anchorKeyCache = anchorKeyCache;
+        }
+
+        // GET api/anchors/5
+        [HttpGet("{anchorNumber}")]
+        public async Task<ActionResult<string>> GetAsync(long anchorNumber)
+        {
+            Console.WriteLine("Get Request in GetAsync()\n");
+            // Get the key if present
+            try
+            {
+                return await this.anchorKeyCache.GetAnchorKeyAsync(anchorNumber);
+            }
+            catch(KeyNotFoundException)
+            {
+                return this.NotFound();
+            }
+        }
+        // GET api/anchors/all
+        [HttpGet("all")]
+        public async Task<ActionResult<List<string>>> GetAllAsync()
+        {
+            List<string> outputList = new List<string>();
+            // Get the all anchors
+            List<AnchorCacheEntity> anchorCacheEntityList = await this.anchorKeyCache.GetAllAnchorKeysAsync();
+            foreach (AnchorCacheEntity cacheEntity in anchorCacheEntityList)
+            {
+                outputList.Add(cacheEntity.RowKey + " : " + cacheEntity.AnchorKey);
+            }
+
+            return outputList;
+        }
+        // GET api/anchors/last
+        [HttpGet("last")]
+        public async Task<ActionResult<string>> GetAsync()
+        {
+            // Get the last anchor
+            string anchorKey = await this.anchorKeyCache.GetLastAnchorKeyAsync();
+
+            if (anchorKey == null)
+            {
+                return "";
+            }
+
+            return anchorKey;
+        }
+
+        // POST api/anchors
+        [HttpPost]
+        public async Task<ActionResult<long>> PostAsync()
+        {
+            string anchorKey;
+            using (StreamReader reader = new StreamReader(this.Request.Body, Encoding.UTF8))
+            {
+                anchorKey = await reader.ReadToEndAsync();
+            }
+
+            //anchorKey = "1";
+
+            if (string.IsNullOrWhiteSpace(anchorKey))
+            {
+                return this.BadRequest();
+            }
+
+            // Set the key and return the anchor number
+            return await this.anchorKeyCache.SetAnchorKeyAsync(anchorKey);
+        }
+    }
+}
---
+++ Data/IAnchorCache.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace SharingService.Data
+{
+    /// <summary>
+    /// An interface representing an anchor key cache.
+    /// </summary>
+    public interface IAnchorKeyCache
+    {
+        /// <summary>
+        /// Determines whether the cache contains the specified anchor identifier.
+        /// </summary>
+        /// <param name="anchorId">The anchor identifier.</param>
+        /// <returns>A <see cref="Task{System.Boolean}"/> containing true if the identifier is found; otherwise false.</returns>
+        Task<bool> ContainsAsync(long anchorId);
+
+        /// <summary>
+        /// Gets the anchor key asynchronously.
+        /// </summary>
+        /// <param name="anchorId">The anchor identifier.</param>
+        /// <returns>The anchor key.</returns>
+        Task<string> GetAnchorKeyAsync(long anchorId);
+
+        /// <summary>
+        /// Gets all anchors in DB
+        /// </summary>
+        /// <returns>The anchor.</returns>
+        Task<List<AnchorCacheEntity>> GetAllAnchorKeysAsync();
+
+        /// <summary>
+        /// Gets the last anchor key asynchronously.
+        /// </summary>
+        /// <returns>The last anchor key stored if available; otherwise, null.</returns>
+        Task<string> GetLastAnchorKeyAsync();
+
+        /// <summary>
+        /// Sets the anchor key asynchronously.
+        /// </summary>
+        /// <param name="anchorKey">The anchor key.</param>
+        /// <returns>An <see cref="Task{System.Int64}"/> representing the anchor identifier.</returns>
+        Task<long> SetAnchorKeyAsync(string anchorKey);
+    }
+}
