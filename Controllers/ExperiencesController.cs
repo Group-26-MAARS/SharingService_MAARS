93f15e1edd96c301f5b8a49bad5e133c769c9535
Dylan Roberts <dtroberts@knights.ucf.edu>
2020-04-14 13:05:55 -0400

    fixed another issue

--- Controllers/ExperiencesController.cs
+++ Controllers/ExperiencesController.cs
@@ -63,6 +63,10 @@ namespace SharingService.Controllers
             // Get the key if present
             try
             {
+                // currExperienceItem will be like the following:
+                //R_ThisRoute, A_MyExperience, R_FinalRoute
+
+                experienceItemsString = await this.experienceKeyCache.GetExperienceKeyAsync(experienceName);
                 string finalReturnStr = "";
                 for (int i = 0; i <= experienceItemsString.Count(x => x == ','); i++)
                 {
